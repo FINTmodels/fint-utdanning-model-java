b56cc7a6d3d92450d37c4f8077c81b6185838a5c
Asgeir Nilsen <asgeir.nilsen@acando.no>
2017-09-25 12:37:00 +0200

    Work around bugs from model generation.

--- src/main/java/no/fint/model/utdanning/kodeverk/Grepkode.java
+++ src/main/java/no/fint/model/utdanning/kodeverk/Grepkode.java
@@ -5,11 +5,9 @@ package no.fint.model.utdanning.kodeverk;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @EqualsAndHashCode
 public class Grepkode {}
 
